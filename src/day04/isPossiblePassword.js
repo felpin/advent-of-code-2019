2cc142bbe2be4652fe770ec0f89f2bd1a9d28872
Felipe de Alencar Pinheiro <felipe.pinheiro.90@hotmail.com>
2020-05-22 00:00:05 -0300

    Day 04, Part 02

--- src/day04/isPossiblePassword.js
+++ src/day04/isPossiblePassword.js
@@ -1,10 +1,23 @@
-const hasSameAdjacentNumber = (values) =>
-  values.some((value, index, all) => index > 0 && value === all[index - 1]);
+const hasSameAdjacentNumberExactTimes = (digits, times = 2) => {
+  const repeat = [];
+
+  digits.forEach((value, index, all) => {
+    if (index === 0 || value !== all[index - 1]) {
+      repeat.push(1);
+    } else {
+      repeat.push(repeat[index - 1] + 1);
+    }
+  });
+
+  return repeat.some((value, index, all) => {
+    return value === times && (index === all.length - 1 || all[index + 1] === 1);
+  });
+};
 
 const onlyIncreases = (values) =>
   values.every((value, index, all) => index === 0 || value >= all[index - 1]);
 
 module.exports = function isPossiblePassword(guess) {
   const guessCharacters = `${guess}`.split("");
-  return hasSameAdjacentNumber(guessCharacters) && onlyIncreases(guessCharacters);
+  return onlyIncreases(guessCharacters) && hasSameAdjacentNumberExactTimes(guessCharacters);
 };
--- src/day04/isPossiblePassword.test.js
+++ src/day04/isPossiblePassword.test.js
@@ -1,13 +1,29 @@
 const isPossiblePassword = require("./isPossiblePassword");
 
-test("111111 meets these criteria", () => {
-  expect(isPossiblePassword(111111)).toBe(true);
+test("111111 does not", () => {
+  expect(isPossiblePassword(111111)).toBe(false);
 });
 
-test("223450 does not meet these criteria", () => {
+test("223450 does not", () => {
   expect(isPossiblePassword(223450)).toBe(false);
 });
 
-test("123789 does not meet these criteria", () => {
+test("123789 does not", () => {
   expect(isPossiblePassword(123789)).toBe(false);
+});
+
+test("112233 does", () => {
+  expect(isPossiblePassword(112233)).toBe(true);
+});
+
+test("123444 does not", () => {
+  expect(isPossiblePassword(123444)).toBe(false);
+});
+
+test("111122 does", () => {
+  expect(isPossiblePassword(111122)).toBe(true);
+});
+
+test("688889 does not", () => {
+  expect(isPossiblePassword(688889)).toBe(false);
 });
